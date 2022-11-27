efaa14c64929863dd5906bc98d7fb1ecbf8c13bc
hanlam12 <118327533+hanlam12@users.noreply.github.com>
2022-11-27 22:38:45 +0700

    Create Taikhoan.js

---
+++ Taikhoan.js
@@ -0,0 +1,8 @@
+function tab() {
+  var tab1 = document.getElementById("form-user");
+  var tab2 = document.getElementById("form-change-pass");
+  var img = document.getElementById("img-profile");
+  tab1.classList.toggle("is-display-none");
+  tab2.classList.toggle("is-display-none");
+  img.classList.toggle("is-display-none");
+}
