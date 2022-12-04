89b617a7bcdf42fbe7766d46cc69e73dcf79a5bb
hanlam12 <118327533+hanlam12@users.noreply.github.com>
2022-12-05 00:45:54 +0700

    Create Quanlyhoso.js

---
+++ Quanlyhoso.js
@@ -0,0 +1,12 @@
+function menupc() {
+  var listpc = document.getElementById("list-menu-pc");
+  listpc.classList.toggle("display");
+}
+function menu() {
+  var list = document.getElementById("list-menu-mb-login");
+  list.classList.toggle("is-left-default");
+}
+function show() {
+  var show = document.getElementById("abi-mb");
+  show.classList.toggle("display-flex");
+}
